ca6418faf979c34e973d750ba5ffbfa56055f914
dong13475 <travel1347@gmail.com>
2023-05-09 11:56:10 +0900

    학습

--- my-vue-city-app/src/components/TopComp.vue
+++ my-vue-city-app/src/components/TopComp.vue
@@ -1,64 +1,85 @@
 <template>
   <header>
-      <ul class="gnb">
-        <!-- v는 값, i는 속성명 -->
-          <li v-for="(v,i) in sdata" v-bind:key="i" v-if="i!='인트로'">
-              <a href="#" v-on:click="chgData(i)">
-                {{ i }}
-              </a>
-          </li>
-          <!-- <li>
-              <a href="#" v-on:click="chgData('부산')">부산</a>
-          </li>
-          <li>
-              <a href="#" v-on:click="chgData('제주')">제주</a>
-          </li> -->
-      </ul>
+    <ul class="gnb">
+      <!-- v는 값, i는 속성명 -->
+      <li v-for="(v, i) in sdata" v-bind:key="i" v-if="i != '인트로'">
+        <a href="#" v-on:click="chgData(i)">
+          {{ i }}
+        </a>
+      </li>
+    </ul>
+    <!-- 메뉴선택이동링크 -->
+    <div class="m2">
+      <!-- v-on:click="" === @click="" -->
+      <a href="#" v-on:click="chgMenu(num)" v-text="'메뉴' + num"></a>
+    </div>
   </header>
 </template>
 
 <script>
 export default {
-  name: 'TopArea',
-  data () {
+  name: "TopArea",
+  data() {
     return {
-      sdata: this.$store.state.cityData
-    }
+      // 1. 도시정보 객체변수
+      sdata: this.$store.state.cityData,
+      // 2. 메뉴번호(처음에 다음메뉴인 2번 넣음)
+      num: 2
+    };
   },
-  methods:{
+  methods: {
     // 스토어 변수 업데이트 메서드
-    chgData(pm){
-      console.log("업데이트!",pm);
+    chgData(pm) {
+      console.log("업데이트!", pm);
       // mutations 메서드 호출하기!
-      this.$store.commit("chgData",pm);
+      this.$store.commit("chgData", pm);
+    },
+    // 메뉴변경하기 메서드
+    chgMenu(n) {
+      // n - 메뉴번호전달
+      console.log("메뉴변경:", n);
+      // mutations 메서드 호출하기!
+      this.$store.commit('chgMenu',n);
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.m2 {
+  display: block;
+  text-align: right;
+}
+.m2 a {
+  font-size: 20px;
+}
+.m2 a:hover {
+  text-decoration: underline;
+  text-decoration-style: double;
+  color: green;
+}
 header {
-    padding: 15px;
-    border: 2px solid #ccc;
+  padding: 15px;
+  border: 2px solid #ccc;
 }
 header ul {
-    margin: 0;
-    padding: 0;
-    list-style: none;
-    display: flex;
-    justify-content: center;
-    gap: 0 15px;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  justify-content: center;
+  gap: 0 15px;
 }
 header a {
-    font-size: 30px;
-    color: #222;
-    text-decoration: none;
+  font-size: 30px;
+  color: #222;
+  text-decoration: none;
 }
 header a:hover,
 header a.on {
-    color: orangered;
-    text-decoration: overline;
-    text-decoration-style: wavy;
+  color: orangered;
+  text-decoration: overline;
+  text-decoration-style: wavy;
 }
 </style>
--- my-vue-city-app/src/main.js
+++ my-vue-city-app/src/main.js
@@ -11,6 +11,7 @@ new Vue({
   components: { App },
   template: '<App/>',
   created(){
+    // created:function(){}를 최대로 줄여서 쓴것
     // 스토어에 있는 initSet 메서드는 어떻게 호출하지?
     // 스토어 호출 메서드가 따로 있음!
     // store.commit("메서드명",파라미터값)
